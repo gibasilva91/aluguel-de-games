da0be101aff96b05b6cbde4edcade72ba81c558e
Gilberto Silva <gilbermariano@gmail.com>
2026-02-04 16:03:00 -0300

    Implementa logica de alugar e devolver

--- js/app.js
+++ js/app.js
@@ -2,5 +2,14 @@ function alterarStatus(id) {
     let gameCLicado = document.getElementById(`game-${id}`);
     let imagem = gameCLicado.querySelector('.dashboard__item__img');
     let botao = gameCLicado.querySelector('.dashboard__item__button');
-    
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
 }
